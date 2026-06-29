9d36975252a70eb292c9d8c925dd458a5bdb0cbf
juliokobayashi-ebury <julio.kobayashi@ebury.com>
2026-06-29 10:39:41 -0300

    fix: updating imports for documentvalidator

--- documentvalidator/documentvalidator.go
+++ documentvalidator/documentvalidator.go
@@ -1,26 +1,24 @@
 package documentvalidator
 
 import (
-	"bexstech/spb-str/internal/platform/documentvalidator/cnpjvalidator"
-	"bexstech/spb-str/internal/platform/documentvalidator/cpfvalidator"
-	"bexstech/spb-str/internal/platform/documentvalidator/documenthelpers"
+	"github.com/juliokobayashi-ebury/lib-teste/documentvalidator/cnpjvalidator"
+	"github.com/juliokobayashi-ebury/lib-teste/documentvalidator/cpfvalidator"
+	"github.com/juliokobayashi-ebury/lib-teste/documentvalidator/documenthelpers"
 )
 
-type DocumentValidator struct{
-	cpfValidator cpfvalidator.CPFValidator
+type DocumentValidator struct {
+	cpfValidator  cpfvalidator.CPFValidator
 	cnpjValidator cnpjvalidator.CNPJValidator
 }
 
 func (ref DocumentValidator) IsValid(data string) bool {
 	data = documenthelpers.SanitizeDocument(data)
-	
+
 	if len(data) == 11 {
 		return ref.cpfValidator.Validate(data)
-	} 
+	}
 	if len(data) == 14 {
 		return ref.cnpjValidator.Validate(data)
 	}
 	return false
 }
-
-
